602cf00dbaaebdb3fff36557b35c9b09c964d863
Arman Sahu <armansahu619@gmail.com>
2026-05-04 15:02:32 +0530

    updated dashboard

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -56,7 +56,7 @@ export function Dashboard() {
       
     
       <div className="md:max-w-7xl max-w-xs  md:px-15 xl:px-0  mx-auto  flex flex-col ">
-        <div className="flex justify-between  items-center py-5"> 
+        <div className="flex md:justify-between justify-center gap-7 md:gap-0  items-center py-5"> 
           <div>
             <div className="flex items-center px-5 md:px-0  gap-2 text-2xl font-semibold">
             {`Welcome, ${username}`}
